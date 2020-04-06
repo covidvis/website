30a410432e1d59b5815949208366ecaf73650106
Stephen Macke <smacke@illinois.edu>
2020-04-06 10:20:37 -0700

    ignore _site again and get rid of docs/

--- docs/scripts/vega_embed.js
+++
@@ -1,7 +0,0 @@
-
-(function(vegaEmbed) {
-  var embedOpt = {"mode": "vega-lite"};
-  vegaEmbed("#country_vis", jhu_country, embedOpt)
-  vegaEmbed("#state_vis", jhu_state, embedOpt)
-})(vegaEmbed);
-    
